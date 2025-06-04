60c4747357e95c6caeca5aacb0c113dd5d125bd2
tobireboredo <tobiasreboredo15@gmail.com>
2025-06-04 11:57:16 -0300

    Texto en grande arriba y tailwind solucionado
    descargar extensiones antes de correr

--- aiafront/src/components/navbar.jsx
+++ aiafront/src/components/navbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const navbar = () => {
+const Navbar = () => {
   return (
-    <div className= 'text-white'>
-        <h1>REACT.</h1>
+    <div className="text-white">
+      <h1 className="w-full text-3xl font-bold text-[#00df9a]">AI Assistant</h1>
     </div>
-  )
-}
+  );
+};
 
-export default navbar
+export default Navbar;
