5ae6203bc5f6b7bf47cb0c6b33d90c30f70a97a5
tobireboredo <tobiasreboredo15@gmail.com>
2025-06-04 12:02:58 -0300

    navbar con mas texto NO FUNCA

--- aiafront/src/components/navbar.jsx
+++ aiafront/src/components/navbar.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className="text-white">
+    <div className="flex justify-between text-white">
       <h1 className="w-full text-3xl font-bold text-[#00df9a]">AI Assistant</h1>
+        <ul className="flex">
+            <li className="p-4">Home</li>
+            <li className="p-4">Chatbot</li>
+            <li className="p-4">About AIA</li>
+        </ul>
     </div>
   );
 };
