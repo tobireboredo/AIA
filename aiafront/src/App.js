5e799f876dd0da8511d1ac4a97fd0fbb10f703bd
tobireboredo <tobiasreboredo15@gmail.com>
2025-06-06 14:18:21 -0300

    logo arriba izq

--- aiafront/src/App.js
+++ aiafront/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import logo from './png/AIA_logo.png'
 
 function App() {
   return (
-    <div>
+    <div className="p-3 bg-black min-h-screen">
       <Navbar />
     </div>
   );
--- aiafront/src/components/navbar.jsx
+++ aiafront/src/components/navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import logo from '../png/AIA_logo.png'
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between text-white">
-      <h1 className="w-full text-3xl font-bold text-[#00df9a]">AI Assistant</h1>
+    <div className="flex justify-between text-white -m4">
+      <img src={logo} alt= "aialogo"></img>
         <ul className="flex">
             <li className="p-4">Home</li>
             <li className="p-4">Chatbot</li>
