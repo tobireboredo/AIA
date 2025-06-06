883047973227e96d88b19bc36569b290db73fb26
tobireboredo <tobiasreboredo15@gmail.com>
2025-06-06 14:28:54 -0300

    boton inicio sesion

--- aiafront/src/components/navbar.jsx
+++ aiafront/src/components/navbar.jsx
@@ -5,11 +5,9 @@ const Navbar = () => {
   return (
     <div className="flex justify-between text-white -m4">
       <img src={logo} alt= "aialogo"></img>
-        <ul className="flex">
-            <li className="p-4">Home</li>
-            <li className="p-4">Chatbot</li>
-            <li className="p-4">About AIA</li>
-        </ul>
+      <button 
+      style={{backgroundColor:'#273C5E'}} 
+      className="text-white px-4 py-2 rounded-full" > Iniciar Sesión</button>
     </div>
   );
 };
