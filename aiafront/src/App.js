5b69890596adf8fce707d1fbce0629742b20077e
tobireboredo <tobiasreboredo15@gmail.com>
2025-06-10 10:16:14 -0300

    imagen grande y ajustes a botones

--- aiafront/src/App.js
+++ aiafront/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import logo from './png/AIA_logo.png'
 
 function App() {
   return (
-    <div className="p-3 bg-black min-h-screen">
+    <div className="p-3 #f5f5f5 min-h-screen">
       <Navbar />
     </div>
   );
--- aiafront/src/components/navbar.jsx
+++ aiafront/src/components/navbar.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import logo from '../png/AIA_logo.png'
+import imgdirectorio from '../png/imgdirectorio.png'
+import tabinicio from '../png/tabinicio.png'
 
 const Navbar = () => {
   return (
     <div className="flex justify-between text-white -m4">
       <img src={logo} alt= "aialogo"></img>
+      
+      <img src={imgdirectorio} alt= "imgdirectorio"
+      className="absolute left-[137px] self-center top-[39px]"></img>
+      
+      <img src={tabinicio} alt= "tabinicio"
+      className="absolute left-[183px] self-center top-0"></img>
+
       <button 
-      style={{backgroundColor:'#273C5E'}} 
-      className="text-white px-4 py-2 rounded-full" > Iniciar Sesión</button>
+      style={{backgroundColor:'#273C5E', width: '162px', height: '39px'}} 
+      className="text-white rounded-full self-center" > Iniciar Sesión</button>
     </div>
   );
 };
